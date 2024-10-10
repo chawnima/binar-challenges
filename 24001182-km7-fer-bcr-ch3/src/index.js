5da79dc068fd616d4e77b93f8df68c7a2cc5c30e
chawnima <mferdyalviansyah@gmail.com>
2024-10-10 20:45:05 +0700

    change port

--- 24001182-km7-fer-bcr-ch3/src/index.js
+++ 24001182-km7-fer-bcr-ch3/src/index.js
@@ -6,7 +6,7 @@ const { errorHandler, notFoundURLHandler } = require("./middlewares/errors");
 const fileUpload = require("express-fileupload");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }));
